b7687214728c582153a528e9c8086c6c7ee996b4
Matt Fredrikson <mattfredrikson@gmail.com>
2021-08-27 14:47:16 -0400

    added 2021 course page

---
+++ 2021/schedule.js
@@ -0,0 +1,201 @@
+schedule = [
+	{
+		"date": "8/31",
+		"topic": "Introduction",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/2",
+		"topic": "Propositional logic &amp; proof",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/7",
+		"topic": "Proving soundness",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/9",
+		"topic": "Program semantics",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/14",
+		"topic": "Safety",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/16",
+		"topic": "Proving safety compositionally",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/21",
+		"topic": "Memory safety",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/23",
+		"topic": "Sandboxing",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/28",
+		"topic": "Security Automata",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/30",
+		"topic": "Verifying end-to-end safety at scale",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/5",
+		"topic": "Non-interference",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/7",
+		"topic": "Information flow policies",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/12",
+		"topic": "Enforcing flow with types",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/14",
+		"topic": "No lecture",
+		"pdf": "",
+		"tex": "",
+		"comments": "Fall break"
+	},
+	{
+		"date": "10/19",
+		"topic": "Simple security &amp; confinement",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/21",
+		"topic": "Relaxing non-interference",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/26",
+		"topic": "Timing side channels",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/28",
+		"topic": "Memory side channels",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/2",
+		"topic": "Privacy policies",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/4",
+		"topic": "Provable privacy",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/9",
+		"topic": "Privacy attacks",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/11",
+		"topic": "Authorization, logically",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/16",
+		"topic": "Bootstrapping trust",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/18",
+		"topic": "Trusted computing",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/23",
+		"topic": "Open office hours",
+		"pdf": "",
+		"tex": "",
+		"comments": "Attendance optional",
+	},
+	{
+		"date": "11/25",
+		"topic": "No lecture",
+		"pdf": "",
+		"tex": "",
+		"comments": "Thanksgiving holiday",
+		"color": "success"
+	},
+	{
+		"date": "11/30",
+		"topic": "Guest lecture",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+	},
+	{
+		"date": "12/3",
+		"topic": "",
+		"pdf": "",
+		"tex": "",
+		"comments": "Last day of classes",
+		"color": "danger"
+	}
+
+]
