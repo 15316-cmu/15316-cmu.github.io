0f71c3c35e7cb0b4cba85567c98705137f212d28
Matt Fredrikson <mattfredrikson@gmail.com>
2022-08-29 16:45:28 -0400

    Added 2022 page

---
+++ 2022/schedule.js
@@ -0,0 +1,217 @@
+schedule = [
+	{
+		"date": "8/30",
+		"topic": "Introduction",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/1",
+		"topic": "Safety &amp; proof",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/6",
+		"topic": "Soundness",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/8",
+		"topic": "Programs &amp; semantics",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/13",
+		"topic": "Dynamic Logic",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/15",
+		"topic": "Proving safety compositionally",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/20",
+		"topic": "Memory safety",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/22",
+		"topic": "Sandboxing",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/27",
+		"topic": "Security automata",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "9/29",
+		"topic": "Enforceable policies &amp; dynamic instrumentation",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/4",
+		"topic": "Non-interference",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/6",
+		"topic": "Information flow policies &amp; security types",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/11",
+		"topic": "Simple security, confinement, \& sound enforcement",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/13",
+		"topic": "Relaxing non-interference",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+	},
+	{
+		"date": "10/18",
+		"topic": "Fall break",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+		"color": "success"
+	},
+	{
+		"date": "10/20",
+		"topic": "Fall break",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+		"color": "success"
+	},
+	{
+		"date": "10/25",
+		"topic": "Timing side channels",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "10/27",
+		"topic": "Memory side channels",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/1",
+		"topic": "Privacy policies",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/3",
+		"topic": "Provable privacy",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/8",
+		"topic": "Privacy attacks",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/10",
+		"topic": "Authorization, logically",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/15",
+		"topic": "Bootstrapping trust",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/17",
+		"topic": "Trusted computing",
+		"pdf": "",
+		"tex": "",
+		"comments": ""
+	},
+	{
+		"date": "11/22",
+		"topic": "Open office hours",
+		"pdf": "",
+		"tex": "",
+		"comments": "Attendance optional",
+		"color": "success"
+	},
+	{
+		"date": "11/24",
+		"topic": "No lecture",
+		"pdf": "",
+		"tex": "",
+		"comments": "Thanksgiving holiday",
+		"color": "success"
+	},
+	{
+		"date": "11/29",
+		"topic": "Special topics",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+	},
+	{
+		"date": "12/1",
+		"topic": "Special topics",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+	},
+	{
+		"date": "12/6",
+		"topic": "Special topics",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+	},
+	{
+		"date": "12/8",
+		"topic": "Final exam review",
+		"pdf": "",
+		"tex": "",
+		"comments": "",
+	}
+
+]
