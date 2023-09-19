ba4097d4b74597659351fd0b9c6f8f969eda6c6d
Matt Fredrikson <mattfredrikson@gmail.com>
2023-09-19 13:26:38 -0400

    added lecture 5

--- 2023/schedule.js
+++ 2023/schedule.js
@@ -44,8 +44,8 @@ schedule = [
 	{
 		"date": "9/19",
 		"topic": "Memory safety",
-		"pdf": "",
-		"tex": "",
+		"pdf": "05-memsandbox.pdf",
+		"tex": "05-memsandbox.tex",
 		"comments": "",
 	},
 	{
