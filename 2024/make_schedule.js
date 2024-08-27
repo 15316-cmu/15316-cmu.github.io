8117f1e89db0a506a042d4077fae0ae5f9a0c6df
Frank Pfenning <fp@cs.cmu.edu>
2024-08-27 09:56:38 -0400

    initialized course pages for fall 2024

--- 2024/make_schedule.js
+++ 2024/make_schedule.js
@@ -19,12 +19,20 @@ for(var i = 0; i < schedule.length; i++) {
     links.append(tex)
   }
 
-  comments = jQuery('<td>' + ent['comments'] + "</td>")
+  comments = jQuery('<td>' + ent['comments'] + '</td>')
 
   ent_container.append(date)
   ent_container.append(topic)
   ent_container.append(links)
-  ent_container.append(comments)
+    ent_container.append(comments)
 
-  $("#schedule").append(ent_container)
+    $("#schedule").append(ent_container)
+
+/*
+    if (i % 2 == 1) {
+        blank = jQuery('<tr> <td colspan=4 style="height: 0px;"></td> </tr>')
+        $("#schedule").append(blank)
+    }
+*/
+
 }
